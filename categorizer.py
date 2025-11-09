1136c20229f41e25dcf264954b77768cb697a46c
Saeed Gholami <saeed.ghollami@gmail.com>
2025-11-10 03:26:41 +0330

    try to fix calendar data.

--- categorizer.py
+++ categorizer.py
@@ -20,32 +20,48 @@ def save_calendar(name: str, calendar):
     with open(CALENDARS_DIR / name, "wb") as f:
         f.write(calendar.to_ical())
 
+def counter(word, words):
+    cal = read_calendar("Work.ics")
+    words = []
+    for event in cal.events:
+        word = event["summary"]
+        words.append(word)
+
+    
+
 def modify(calendar, keyword, summary, area="", project="", tags="", detail=""):
     cal = Calendar()
+    description = ""
     for event in calendar.events:
         event_copy = event.copy()
-
-        print(event["summary"])
         if keyword in event.get("summary", "").lower():
+            print('true')
             event_copy["summary"] = summary
             description = f"Area: {area}\nProject: {project}\nTags: {tags}\nDetail: {detail}"
             event_copy["description"] = description
+        # else:
+        #     description = f"Area:\nProject:\nTags:\nDetail:\n"
+        #     print('false')
         cal.add_component(event_copy)
     return cal
 
 
 def main():
-    cal = read_calendar("Work.ics")
-    data = [
+    rows = [
         ("gcal", "Develop gcal", "Dev", "gcal", "Python, DataSci", "Google calendar visualizer to get insight."),
         ("pyquiz", "Develop pyquiz", "Dev", "pyquiz", "Javascript, Web", "Quiz application to determine level of the participent."),
         ("farnaz", "Teaching Farnaz", "Teaching", "Teaching Farnaz", "Python, DataSci", "Teaching Farnaz Shasti."),
         ("arad", "Teaching Arad", "Teaching", "Teaching Arad", "Python", "Teaching Arad."),
         ("motlag", "Teaching motlag", "Teaching", "Teaching Motlag", "Python, Django", "Teaching Mina Motlag."),
-        # ("motlag", "Teaching motlag", "Teaching", "Teaching Motlag", "Python, Django", "Teaching Mina Motlag.")
+        ("random", "Random projects", "Dev", "Random projects", "Python", "Random project just fill the calendar."),
+        ("coding signal", "Signal plot", "Dev", "Signal Plot", "Python, Matplotlib", "Signal visulaizer for Nastran files."),
+        ("flet", "Learn Flet", "Dev", "Learn Flet", "Learn, Flet", "Learn the basic of the flet framework"),
+        ("vim", "Modify neovim", "Dev", "Learn vim/neovim", "Tools, neovim", "Modifiy or learn vim/neovim"),
     ]
-    new_cal = modify(cal, "pyquiz", "Develop pyquiz", "Dev", "pyquiz", "Javascript, Web", "Quiz application to determine level of the participent")
-    # save_calendar("temp.ics", new_cal)
+    for row in rows:
+        cal = read_calendar("temp.ics")
+        new_cal = modify(cal, *row)
+        save_calendar("temp.ics", new_cal)
 
 
 
